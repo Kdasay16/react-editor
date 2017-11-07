26cf23eeb206c0bae4aee7cfdf6cd7ed89b056b4
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-11-07 14:11:26 -0800

    css

--- lab-kayla/src/App.js
+++ lab-kayla/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import './App.css'
 
 class App extends Component {
 
   constructor(){
     super()
     this.state = {
+      name: 'Kayla',
       words: 'I like cows',
       color: '#4B2840',
-      name: 'Kayla'
+      photo: 'logo.svg'
     }
   }
 
+  handleNamechange(value) {
+    this.setState({ name: value })
+  }
   handleWordsChange(value) {
     this.setState({ words: value })
   }
@@ -19,21 +24,21 @@ class App extends Component {
     this.setState({ color: value })
   }
 
-  handleNamechange(value) {
-    this.setState({ name: value })
+  handlePhotoChange(value) {
+    this.setState({ photo: value})
   }
 
   render() {
-    const { words, color, name } = this.state
+    const { name, words, color } = this.state
 
     return (
       <div className="userControl">
         <Controls words={words} color={color} name={name}
+          onNameChange={name => this.handleNamechange(name)}
           onWordsChange={words => this.handleWordsChange(words)}
           onColorChange={color => this.handleColorChange(color)}
-          onNameChange={name => this.handleNamechange(name)}
         />
-
+        <Speak words={words} color={color} name={name} specialClass="weird" />
       </div>
     )
   }
@@ -41,22 +46,38 @@ class App extends Component {
 
 class Controls extends Component {
   render() {
-    const { words, color, name, onWordsChange, onColorChange, onNameChange} = this.props
+    const { name, words, color, onWordsChange, onColorChange, onNameChange} = this.props
 
     return(
       <div>
-        <label>
+        <label className="name-label">
+        name:
+        <input name="name" value={name} onChange={({ target }) => onNameChange(target.value)} />
+        </label>
+        <br/>
+        <label className="words-label">
           words:
-          <input name="words" value={words} onChange={({ target }) => onWordsChange(target.value)}/>
+          <input name="words" value={words} onChange={({ target }) => onWordsChange(target.value)} />
         </label>
-        <label>
+        <br/>
+        <label className="color-label">
           color:
-          <input name="color" type="color" value={color} onChange={({ target }) => onColorChange(target.value)}/>
+          <input name="color" type="color" value={color} onChange={({ target }) => onColorChange(target.value)} />
         </label>
-        <label>
-          name:
-          <input name="name" value={name} onChange={({ target }) => onNameChange(target.value)}/>
-        </label>
+      </div>
+    )
+  }
+}
+
+class Speak extends Component {
+  render() {
+    const {name, words, color, specialClass} = this.props
+
+    return(
+      <div className={specialClass} style={{ color }}>
+        <span>{words}</span>
+        &nbsp;
+        <span>{name}</span>
       </div>
     )
   }
